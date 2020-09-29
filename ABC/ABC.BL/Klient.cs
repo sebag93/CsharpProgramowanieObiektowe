739a70f7f544473a6858d6ff9f48f5093a3b31e7
Sebastian Guzdek <sebag93@gmail.com>
2020-09-29 13:15:08 +0200

    3.4 Budowanie klasy - właściwości

---
+++ ABC/ABC.BL/Klient.cs
@@ -0,0 +1,36 @@
+﻿namespace ABC.BL
+{
+    public class Klient
+    {
+        private string _nazwisko;
+
+        public string Nazwisko
+        {
+            get
+            {
+                // tutaj dowolny kod
+                return _nazwisko;
+            }
+            set
+            {
+                // tutaj dowolny kod
+                _nazwisko = value;
+            }
+        }
+
+        public string Imie { get; set; }
+
+        public string Email { get; set; }
+
+        public int KlientId { get; private set; }
+
+        public string ImieNazwisko
+        {
+            get
+            {
+                return Nazwisko + "," + Imie;
+            }
+        }
+
+    }
+}
