14a4faf79618ebabe477aa980535efe4bdff6135
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 01:27:37 +0200

    4.6 Klasa Zamówienie

---
+++ ABC/ABC.BL/Zamowienie.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace ABC.BL
+{
+    public class Zamowienie
+    {
+        public Zamowienie()
+        {
+
+        }
+
+        public Zamowienie(int zamowienieId)
+        {
+            ZamowienieId = zamowienieId;
+        }
+
+        public int ZamowienieId { get; private set; }
+
+        public DateTimeOffset? DataZamowienia { get; set; }
+
+        /// <summary>
+        /// Pobieramy jedno zamówienie
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            // Kod, który pobiera zdefiniowane zamówienie
+            return new Zamowienie();
+        }
+
+        /// <summary>
+        /// Zapisujemy bieżące zamówienie
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // Kod, który zapisuje zdefiniowane zamówienie
+            return true;
+        }
+
+        /// <summary>
+        /// Sprawdzamy dane zamówienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zwaliduj()
+        {
+            var poprawne = true;
+            if (DataZamowienia == null)
+                poprawne = false;
+
+            return poprawne;
+        }
+
+    }
+}
