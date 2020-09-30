2a657af1bcdd711ce906a2863386df846b4f9942
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 14:34:25 +0200

    5.8 Zamówienie Repozytorium

---
+++ ABC/ABC.BL/ZamowienieRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ABC.BL
+{
+    public class ZamowienieRepository
+    {
+        /// <summary>
+        /// Pobieramy jedno zamówienie
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            // Tworzymy instancję klasy zamówienia i przekazujemy identyfikator
+            Zamowienie zamowienie = new Zamowienie(zamowienieId);
+
+            // Kod, który pobiera zdefiniowane zamówienie
+
+            // Tymczasowo zakodowane wartości zamówienia do zwrócenia
+            if (zamowienieId == 10)
+            {
+                zamowienie.DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+            }
+
+            return zamowienie;
+        }
+
+        /// <summary>
+        /// Zapisujemy bieżące zamówienie
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // Kod, który zapisuje zdefiniowane zamówienie
+            return true;
+        }
+
+    }
+}
