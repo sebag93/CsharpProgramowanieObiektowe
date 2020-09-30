dc0dda0a7ee6f5518a4df7090da40081e3e11b2c
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 20:42:33 +0200

    6.10 Budowanie klas o jednym celu

---
+++ ABC/ABC.BL/WyswietlaniePozycjiZamowienia.cs
@@ -0,0 +1,13 @@
+﻿namespace ABC.BL
+{
+    public class WyswietlaniePozycjiZamowienia
+    {
+        public int PozycjaZamowieniaId { get; set; }
+
+        public int IloscZamowienia { get; set; }
+
+        public string NazwaProduktu { get; set; }
+
+        public decimal? CenaZakupu { get; set; }
+    }
+}
--- ABC/ABC.BL/ZamowienieRepository.cs
+++ ABC/ABC.BL/ZamowienieRepository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ABC.BL
 {
@@ -23,6 +24,61 @@ namespace ABC.BL
             }
 
             return zamowienie;
+        }
+
+        /// <summary>
+        /// Pobieramy jedno zamówienie do wyświetlenia
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public WyswietlanieZamowienia  PobierzZamowienieDoWyswietlenia(int zamowienieId)
+        {
+            WyswietlanieZamowienia wyswietlanieZamowienia = new WyswietlanieZamowienia();
+
+            // Kod, który pobiera zdefiniowane pola zamówienia
+
+            // Tymczasowe dane zakodowane na stałe
+            if (zamowienieId == 10)
+            {
+                wyswietlanieZamowienia.Imie = "Marcin";
+                wyswietlanieZamowienia.Nazwisko = "Nowak";
+                wyswietlanieZamowienia.DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0));
+                wyswietlanieZamowienia.AdresDostawy = new Adres()
+                {
+                    AdresTyp = 1,
+                    Ulica = "Kosmiczna",
+                    Miasto = "Katowice",
+                    KodPocztowy = "40-467",
+                    Kraj = "Polska"
+                };
+            }
+            wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista = new List<WyswietlaniePozycjiZamowienia>();
+
+            // Kod, który pobiera elementy zamówienia
+
+            // Tymczasowe dane zakodowane na stałe
+            if (zamowienieId == 10)
+            {
+                var wyswietlaniePozycjiZamowienia = new WyswietlaniePozycjiZamowienia()
+                {
+                    NazwaProduktu = "Krzesło",
+                    IloscZamowienia = 4,
+                    CenaZakupu = 119.77M
+                };
+
+                wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista.Add(wyswietlaniePozycjiZamowienia);
+
+                wyswietlaniePozycjiZamowienia = new WyswietlaniePozycjiZamowienia()
+                {
+                    NazwaProduktu = "Stolik",
+                    IloscZamowienia = 7,
+                    CenaZakupu = 249M
+                };
+
+                wyswietlanieZamowienia.WyswietlaniePozycjiZamowieniaLista.Add(wyswietlaniePozycjiZamowienia);
+            }
+
+            return wyswietlanieZamowienia;
         }
 
         /// <summary>
