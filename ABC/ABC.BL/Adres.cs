5e43c87423c61bea88f5e43a57961a4553c816f1
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 13:32:29 +0200

    5.3 Klasa Adres

---
+++ ABC/ABC.BL/Adres.cs
@@ -0,0 +1,27 @@
+﻿namespace ABC.BL
+{
+    public class Adres
+    {
+        public Adres()
+        {
+
+        }
+
+        public Adres(int adresId)
+        {
+            AdresId = adresId;
+        }
+
+        public int AdresId { get; private set; }
+
+        public int AdresTyp { get; set; }
+
+        public string Ulica { get; set; }
+
+        public string Miasto { get; set; }
+
+        public string KodPocztowy { get; set; }
+
+        public string Kraj { get; set; }
+    }
+}
