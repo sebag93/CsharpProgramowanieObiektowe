2e86d0af6907488b3e43f42567a439291fbb0327
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 14:28:09 +0200

    5.7 Produkt Repozytorium Test

---
+++ ABC/ABC.BLTest/ProduktRepositoryTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ProduktRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzProdukt()
+        {
+            // Arrange
+            var produktRepository = new ProduktRepository();
+            var oczekiwana = new Produkt(2)
+            {
+                NazwaProduktu = "Klocki",
+                Opis = "Klocki do zabawy dla dzieci powyżej 3 lat",
+                AktualnaCena = 119.99M
+            };
+
+            // Act
+            var aktualna = produktRepository.Pobierz(2);
+
+            // Assert
+            Assert.AreEqual(oczekiwana.NazwaProduktu, aktualna.NazwaProduktu);
+            Assert.AreEqual(oczekiwana.Opis, aktualna.Opis);
+            Assert.AreEqual(oczekiwana.AktualnaCena, aktualna.AktualnaCena);
+
+        }
+    }
+}
