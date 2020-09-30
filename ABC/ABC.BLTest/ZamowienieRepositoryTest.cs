8069c1c7b3ccf8f8a49758717520580d38d13ff6
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 14:46:48 +0200

    5.9 Zamówienie Repozytorium Test

---
+++ ABC/ABC.BLTest/ZamowienieRepositoryTest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using ABC.BL;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ABC.BLTest
+{
+    [TestClass]
+    public class ZamowienieRepositoryTest
+    {
+        [TestMethod]
+        public void PobierzZamowienie()
+        {
+            // Arrange
+            var zamowienieRepository = new ZamowienieRepository();
+            var oczekiwana = new Zamowienie(10)
+            {
+                DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0))
+            };
+
+            // Act
+            var aktualna = zamowienieRepository.Pobierz(10);
+
+            // Assert
+            Assert.AreEqual(oczekiwana.ZamowienieId, aktualna.ZamowienieId);
+            Assert.AreEqual(oczekiwana.DataZamowienia, aktualna.DataZamowienia);
+        }
+    }
+}
