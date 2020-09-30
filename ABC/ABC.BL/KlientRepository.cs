6dcc90b3d74febd7ad11fbcae6a787918b220bc1
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 13:39:30 +0200

    5.4 Klient Repozytorium

---
+++ ABC/ABC.BL/KlientRepository.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections.Generic;
+
+namespace ABC.BL
+{
+    public class KlientRepository
+    {
+        /// <summary>
+        /// Pobieramy jednego klienta
+        /// </summary>
+        /// <param name="klientId"></param>
+        public Klient Pobierz(int klientId)
+        {
+            //Tworzymy instancję klasy klienta
+            Klient klient = new Klient(klientId);
+
+            // kod który pobiera określonego klienta
+
+            //Tymczasowo zakodowane wartości aby zwrócić klienta
+            if (klientId == 1)
+            {
+                klient.Email = "marcin@dev-hobby.pl";
+                klient.Imie = "Marcin";
+                klient.Nazwisko = "Nowak";
+            }
+
+
+            return new Klient();
+        }
+
+        /// <summary>
+        /// Pobieramy wszystkich klientów
+        /// </summary>
+        public List<Klient> Pobierz()
+        {
+            // kod który pobiera wszystkich klientów
+            return new List<Klient>();
+        }
+
+        /// <summary>
+        /// Zapisujemy obecnego klienta
+        /// </summary>
+        public bool Zapisz()
+        {
+            // kod który zapisuje zdefiniowanego klienta
+            return true;
+        }
+    }
+}
