4c16d6b9ec93e96a8fc54d037717e45ac2cdfb7f
Sebastian Guzdek <sebag93@gmail.com>
2020-09-30 21:12:14 +0200

    6.11 Kolejne testy jednostkowe

--- ABC/ABC.BLTest/ZamowienieRepositoryTest.cs
+++ ABC/ABC.BLTest/ZamowienieRepositoryTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using ABC.BL;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -23,6 +24,64 @@ namespace ABC.BLTest
             // Assert
             Assert.AreEqual(oczekiwana.ZamowienieId, aktualna.ZamowienieId);
             Assert.AreEqual(oczekiwana.DataZamowienia, aktualna.DataZamowienia);
+        }
+
+        [TestMethod]
+        public void PobierzZamowienieDoWyswietleniaTest()
+        {
+            // Arrange
+            var zamowienieRepository = new ZamowienieRepository();
+            var oczekiwana = new WyswietlanieZamowienia()
+            {
+                Imie = "Marcin",
+                Nazwisko = "Nowak",
+                DataZamowienia = new DateTimeOffset(2018, 4, 14, 10, 00, 00, new TimeSpan(7, 0, 0)),
+                AdresDostawy = new Adres()
+                {
+                    AdresTyp = 1,
+                    Ulica = "Kosmiczna",
+                    Miasto = "Katowice",
+                    KodPocztowy = "40-467",
+                    Kraj = "Polska"
+                },
+                WyswietlaniePozycjiZamowieniaLista = new List<WyswietlaniePozycjiZamowienia>()
+                {
+                    new WyswietlaniePozycjiZamowienia()
+                    {
+                        NazwaProduktu = "Krzesło",
+                        IloscZamowienia = 4,
+                        CenaZakupu = 119.77M
+                    },
+                    new WyswietlaniePozycjiZamowienia()
+                    {
+                        NazwaProduktu = "Stolik",
+                        IloscZamowienia = 7,
+                        CenaZakupu = 249M
+                    }
+                }
+            };
+
+            // Act
+            var aktualna = zamowienieRepository.PobierzZamowienieDoWyswietlenia(10);
+
+            // Assert
+            Assert.AreEqual(oczekiwana.ZamowienieId, aktualna.ZamowienieId);
+            Assert.AreEqual(oczekiwana.DataZamowienia, aktualna.DataZamowienia);
+            Assert.AreEqual(oczekiwana.Imie, aktualna.Imie);
+            Assert.AreEqual(oczekiwana.Nazwisko, aktualna.Nazwisko);
+
+            Assert.AreEqual(oczekiwana.AdresDostawy.AdresTyp, aktualna.AdresDostawy.AdresTyp);
+            Assert.AreEqual(oczekiwana.AdresDostawy.Ulica, aktualna.AdresDostawy.Ulica);
+            Assert.AreEqual(oczekiwana.AdresDostawy.Miasto, aktualna.AdresDostawy.Miasto);
+            Assert.AreEqual(oczekiwana.AdresDostawy.KodPocztowy, aktualna.AdresDostawy.KodPocztowy);
+            Assert.AreEqual(oczekiwana.AdresDostawy.Kraj, aktualna.AdresDostawy.Kraj);
+
+            for (int i = 0; i < 1; i++)
+            {
+                Assert.AreEqual(oczekiwana.WyswietlaniePozycjiZamowieniaLista[i].NazwaProduktu, aktualna.WyswietlaniePozycjiZamowieniaLista[i].NazwaProduktu);
+                Assert.AreEqual(oczekiwana.WyswietlaniePozycjiZamowieniaLista[i].IloscZamowienia, aktualna.WyswietlaniePozycjiZamowieniaLista[i].IloscZamowienia);
+                Assert.AreEqual(oczekiwana.WyswietlaniePozycjiZamowieniaLista[i].CenaZakupu, aktualna.WyswietlaniePozycjiZamowieniaLista[i].CenaZakupu);
+            }
         }
     }
 }
